4f97f9f95a7ddcd4546fd08a7740dc07fd6955a2
Dakari Antoine <dakariantoine@Dakaris-MacBook-Pro.local>
2020-07-16 17:17:27 -0500

    Added voice assistant

--- coronavirus.py
+++ coronavirus.py
@@ -1,12 +1,57 @@
 import requests
 import json
+import pyttsx3
+import speech_recognition as sr
+import re
 
 API_KEY = "tXEYmAxK6EZD"
 PROJECT_TOKEN = "tFMA3o46cafo"
 RUN_TOKEN = "tZXQLyYhnOZb"
 
-response = requests.get(
-    f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
-data = json.loads(response.text)
+class Data:
+    def __init__(self, api_key, project_token):
+        self.api_key = api_key
+        self.project_token = project_token
+        self.params = {
+            "api_key": self.api_key
+        }
+        self.get_data()
+    
+    def get_data(self):
+        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.params)
+        self.data = json.loads(response.text)
 
-print(data)
+    def get_total_cases(self):
+        data = self.data['total']
+
+        for content in data:
+            if content['name'] == "Coronavirus Cases:":
+                return content['value']
+
+    def get_total_deaths(self):
+        data = self.data['total']
+
+        for content in data:
+            if content['name'] == "Deaths:":
+                return content['value']
+
+        return "0"
+
+    def get_country_data(self, country):
+        data = self.data["country"]
+
+        for content in data:
+            if content['name'].lower() == country.lower():
+                return content
+
+        return "0"
+
+data = Data(API_KEY, PROJECT_TOKEN)
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+speak("hello")
