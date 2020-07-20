18cd67793240d07d8274e3dab2c879c694bcd98d
Dakari Antoine <dakariantoine@Dakaris-MBP.lan1>
2020-07-20 01:15:52 -0500

    Added asking by country capability/automatic update to stats

--- coronavirus.py
+++ coronavirus.py
@@ -3,6 +3,8 @@ import json
 import pyttsx3
 import speech_recognition as sr
 import re
+import threading
+import time
 
 API_KEY = "tXEYmAxK6EZD"
 PROJECT_TOKEN = "tFMA3o46cafo"
@@ -15,11 +17,12 @@ class Data:
         self.params = {
             "api_key": self.api_key
         }
-        self.get_data()
+        self.data = self.get_data()
     
     def get_data(self):
         response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.params)
-        self.data = json.loads(response.text)
+        data = json.loads(response.text)
+        return data
 
     def get_total_cases(self):
         data = self.data['total']
@@ -53,7 +56,24 @@ class Data:
 
         return countries
 
+    def update_data(self):
+        response = requests.post(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/run', params=self.params)
+        
+        #This function will update the data automatically so it doesn't have to be done manually each new day
+        def poll():
+            time.sleep(0.1)
+            old_data = self.data
+            while True:
+                new_data = self.get_data()
+                if new_data != old_data:
+                    self.data = new_data
+                    print("Data updated")
+                    break
+                time.sleep(5)
 
+        
+        t = threading.Thread(target=poll)
+        t.start()
 
 def speak(text):
     engine = pyttsx3.init()
@@ -77,8 +97,8 @@ def get_audio():
 def main():
     print("Started Program")
     data = Data(API_KEY, PROJECT_TOKEN)
-
-    END_PHRASE = "Stop"
+    END_PHRASE = "stop"
+    country_list = (data.get_list_of_countries())
 
     TOTAL_PATTERNS = {
                     re.compile("[\w\s]+ total [\w\s]+ cases"):data.get_total_cases,
@@ -87,19 +107,40 @@ def main():
                     re.compile("[\w\s]+ total deaths"):data.get_total_deaths,
                     }
 
+    COUNTRY_PATTERNS = {
+                    re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+                    re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],                    
+                        }
+    UPDATE_COMMAND = "update"
+
     while True:
         print("Listening...")
         text = get_audio()
+        print(text)
         result = None
 
+        for pattern, func in COUNTRY_PATTERNS.items():
+            if pattern.match(text):
+                words = set(text.split(" "))
+                for country in country_list:
+                    if country in words:
+                        result = func(country)
+                        break
+        
         for pattern, func in TOTAL_PATTERNS.items():
             if pattern.match(text):
                 result = func()
                 break
+
+        if text == UPDATE_COMMAND:
+            result = "Data is being updated. This may take a moment, please be patient!" 
+            data.update_data()
+
         if result:
             speak(result)
 
-        if text.find(END_PHRASE): #stop loop
+        if text.find(END_PHRASE) != -1: #stop loop
+            print("Exit")
             break
-
+        
 main()
